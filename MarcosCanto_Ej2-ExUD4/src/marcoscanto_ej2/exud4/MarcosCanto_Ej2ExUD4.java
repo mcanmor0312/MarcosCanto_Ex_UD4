1ca55dbe3ed5bcf3878b243a769bb06b2451ab49
mcanmor0312 <mcanmor0312@g.educaand.es>
2024-02-22 12:02:53 +0100

    Primera revision 1

---
+++ MarcosCanto_Ej2-ExUD4/src/marcoscanto_ej2/exud4/MarcosCanto_Ej2ExUD4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package marcoscanto_ej2.exud4;
+
+/**
+ *
+ * @author mcanmor0312
+ */
+public class MarcosCanto_Ej2ExUD4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
